c0994825e626ab171deb357c86aa65730d1c6e85
Nat <nat@cmmc.io>
2017-01-13 18:26:43 +0700

    Add mqtt button.

---
+++ sample-button-mqtt/app/src/main/java/com/example/androidthings/button/ButtonActivity.java
@@ -0,0 +1,207 @@
+/*
+ * Copyright 2016, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.androidthings.button;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+
+import com.google.android.things.contrib.driver.button.Button;
+import com.google.android.things.contrib.driver.button.ButtonInputDriver;
+import com.google.android.things.pio.Gpio;
+import com.google.android.things.pio.PeripheralManagerService;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import java.io.IOException;
+
+/**
+ * Example of using Button driver for toggling a LED.
+ * <p>
+ * This activity initialize an InputDriver to emit key events when the button GPIO pin state change
+ * and flip the state of the LED GPIO pin.
+ * <p>
+ * You need to connect an LED and a push button switch to pins specified in {@link BoardDefaults}
+ * according to the schematic provided in the sample README.
+ */
+public class ButtonActivity extends Activity {
+    private static final String TAG = ButtonActivity.class.getSimpleName();
+
+    private Gpio mLedGpio;
+    private ButtonInputDriver mButtonInputDriver;
+    private MqttAndroidClient mqttAndroidClient;
+    final String serverUri = "tcp://mqtt.cmmc.io:1883";
+    final String clientId = "ExampleAndroidClient" + System.currentTimeMillis() + Math.random();
+    final String subscriptionTopic = "led001";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setupMqttClient();
+        Log.i(TAG, "Starting ButtonActivity");
+
+        PeripheralManagerService pioService = new PeripheralManagerService();
+        try {
+            Log.i(TAG, "Configuring GPIO pins");
+            mLedGpio = pioService.openGpio(BoardDefaults.getGPIOForLED());
+            mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+
+            Log.i(TAG, "Registering button driver");
+            // Initialize and register the InputDriver that will emit SPACE key events
+            // on GPIO state changes.
+            mButtonInputDriver = new ButtonInputDriver(
+                    BoardDefaults.getGPIOForButton(),
+                    Button.LogicState.PRESSED_WHEN_LOW,
+                    KeyEvent.KEYCODE_SPACE);
+            mButtonInputDriver.register();
+        } catch (IOException e) {
+            Log.e(TAG, "Error configuring GPIO pins", e);
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_SPACE) {
+            // Turn on the LED
+            setLedValue(true);
+            publish("test001", "ON");
+            Log.d(TAG, "Press");
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_SPACE) {
+            // Turn off the LED
+            setLedValue(false);
+            return true;
+        }
+
+        return super.onKeyUp(keyCode, event);
+    }
+
+    /**
+     * Update the value of the LED output.
+     */
+    private void setLedValue(boolean value) {
+        try {
+            mLedGpio.setValue(value);
+        } catch (IOException e) {
+            Log.e(TAG, "Error updating GPIO value", e);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (mButtonInputDriver != null) {
+            mButtonInputDriver.unregister();
+            try {
+                mButtonInputDriver.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Error closing Button driver", e);
+            } finally {
+                mButtonInputDriver = null;
+            }
+        }
+
+        if (mLedGpio != null) {
+            try {
+                mLedGpio.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Error closing LED GPIO", e);
+            } finally {
+                mLedGpio = null;
+            }
+            mLedGpio = null;
+        }
+    }
+
+    private void publish(String topic, String payload) {
+        try {
+            mqttAndroidClient.publish(topic, new MqttMessage(payload.getBytes()));
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void subscribe(String topic) {
+        int qos = 0;
+        try {
+            mqttAndroidClient.subscribe(topic, qos);
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setupMqttClient() {
+        mqttAndroidClient = new MqttAndroidClient(getApplicationContext(), serverUri, clientId);
+        mqttAndroidClient.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean reconnect, String serverURI) {
+                Log.d(TAG, "connectComplete: ");
+                subscribe("test");
+            }
+
+            @Override
+            public void connectionLost(Throwable cause) {
+                Log.d(TAG, "connectionLost: ");
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                Log.d(TAG, "messageArrived: " + message.toString());
+                if (topic.equals("led001")) {
+                    if (message.toString().equals("ON")) {
+                        setLedValue(true);
+                    } else if (message.toString().equals("OFF")) {
+                        setLedValue(false);
+                    }
+                    else {
+                        Log.d(TAG, "messageArrived: INVALID MQTT MSG");
+                    }
+                }
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+                Log.d(TAG, "deliveryComplete: ");
+            }
+        });
+
+        MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
+        mqttConnectOptions.setAutomaticReconnect(true);
+        mqttConnectOptions.setCleanSession(true);
+
+
+        try {
+            mqttAndroidClient.connect(mqttConnectOptions, null);
+        } catch (MqttException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
